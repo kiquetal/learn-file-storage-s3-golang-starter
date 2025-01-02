99b24bf26ff8d45935e251426151647728293828
kiquetal <kiquetal@gmail.com>
2025-01-02 12:43:37 -0300

    Adding generation of 32bit random letter

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"mime"
 	"net/http"
 )
 
@@ -45,6 +48,43 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusUnauthorized, "User not authorized", nil)
 		return
 	}
+
+	//parse video from request
+
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form", err)
+		return
+
+	}
+	file, header, err := r.FormFile("video")
+	if err != nil {
+
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	fmt.Printf("the video has the length of %v\n", file)
+
+	defer file.Close()
+
+	//check header content type
+	headerContentType := header.Header.Get("Content-Type")
+
+	mime.ParseMediaType(headerContentType)
+	fmt.Println("Content Type: ", headerContentType)
+
+	//generate 32bit using random
+
+	var randomId []byte = make([]byte, 32)
+	_, err = rand.Read(randomId)
+	if err != nil {
+		return
+	}
+
+	var stringBase64 = base64.StdEncoding.EncodeToString(randomId)
+	fmt.Println("Random ID: ", stringBase64)
 
 	fmt.Printf("uploading video for video %v by user %v\n", videoID, userID)
 }
